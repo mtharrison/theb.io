5fd1e8161580e1e4282f690eaf9c94a2c31bd71f
Matt Harrison <mt.harrison86@gmail.com>
2014-08-18 22:01:18 +0100

    Updated deploy script to swap out prod config

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,9 @@
+mv config/secrets.json config/x_secrets.json
+mv config/secrets_prod.json config/secrets.json
 docker build -t mtharrison/theb.io .
 docker push mtharrison/theb.io
 ssh theb.io 'sudo docker kill $(sudo docker ps -q)'
 ssh theb.io 'sudo docker pull mtharrison/theb.io'
 ssh theb.io 'sudo docker run -p 80:80 -d -v /home/ubuntu/logs:/home/ubuntu/logs mtharrison/theb.io'
+mv config/secrets.json config/secrets_prod.json
+mv config/x_secrets.json config/secrets.json
